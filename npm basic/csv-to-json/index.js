da66210e9a651df1441cf266f88dde099d09c876
PHUC C  NGO <kevinngo@PHUCs-MBP.ddnszone.calstatela.edu>
2019-11-04 07:11:12 -0800

    Update index.js

--- npm basic/csv-to-json/index.js
+++ npm basic/csv-to-json/index.js
@@ -1,24 +1,3 @@
-// const csvFilePath = './customer-data.csv'
-// const fs = require('fs')
-// const csv = require('csvtojson')
-// let arr = []
-
-// csv()
-//     .fromFile(csvFilePath)
-//     .on('json', (jsonObj) => {
-//         arr.push(jsonObj)
-//     })
-//     .on('done', (error) => {
-//         if (error) return process.exit(1)
-//         console.log(arr)
-//         fs.writeFile('customer-data.json', JSON.stringify(arr, null, 2), (error) => {
-//             if (error) return process.exit(1)
-//             console.log('done')
-//             process.exit(0)
-//         })
-//     })
-
-
 const fs = require('fs')
 const csv = require('csvtojson/v1')
 const path = require('path')
