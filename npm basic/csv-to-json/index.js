aa8510edaf8379d9d5e5aba2d8a50e6968226ae7
PHUC C  NGO <kevinngo@PHUCs-MBP.ddnszone.calstatela.edu>
2019-11-04 07:04:20 -0800

    updated using of csvtojon

--- npm basic/csv-to-json/index.js
+++ npm basic/csv-to-json/index.js
@@ -18,22 +18,26 @@
 //         })
 //     })
 
-const csvFilePath = './customer-data.csv'
-const fs = require('fs')
-const csv = require('csvtojson')
-let arr = []
 
-csv().fromFile(csvFilePath).on('json',(jsonObj)=>{
-    console.log("dsds")
-    arr.push(jsonObj)
-    
-  }).on('done',(error)=>{
-    if (error) return process.exit(1)
-    console.log(arr)
-    fs.writeFile('customer-data.json', JSON.stringify(arr, null, 2), (error)=>{
-      if (error) return process.exit(1)
-      console.log('done')
-      process.exit(0)
+const fs = require('fs')
+const csv = require('csvtojson/v1')
+const path = require('path')
+
+const csvFilePath =  './customer-data.csv';
+let arr = []
+console.log(csvFilePath)
+csv()
+    .fromFile(csvFilePath)
+    .on('json',(jsonObj) => {
+        console.log(jsonObj)
+        arr.push(jsonObj)
     })
-  }
-)
+    .on('done',(error)=>{
+        if (error) return process.exit(1)
+        console.log(arr)
+        fs.writeFile('customer-data.json', JSON.stringify(arr, null, 2), (error)=>{
+            if (error) return process.exit(1)
+            console.log('done')
+            process.exit(0)
+        })
+    })
